dfcf20f7441ce2a42c296baf0daa75ae9785d072
Valdis <valdisxp1@gmail.com>
2016-07-05 22:09:34 +0300

    added key file validation

--- update_keys.sh
+++ update_keys.sh
@@ -11,8 +11,21 @@ then
 	exit 1
 fi
 
-echo "${AUTHORIZED_KEYS}" | tr , "\n" > /root/.ssh/authorized_keys
+echo "${AUTHORIZED_KEYS}" | tr , "\n" > /root/.ssh/authorized_keys.new
+
+# check if the keys are valid
+ssh-keygen -lf /root/.ssh/authorized_keys.new
+if [ $? -ne 0 ]
+then
+	>&2 echo "ERROR: the new authorized_keys file are not valid, SSH keys will not be changed"
+	exit 1
+fi
+
+# authorized_keys file is valid, replace the old one
+mv /root/.ssh/authorized_keys /root/.ssh/authorized_keys.old 2>/dev/null
+mv /root/.ssh/authorized_keys.new /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
+
 echo "Authorized keys:"
 cat /root/.ssh/authorized_keys
 echo "------------"
