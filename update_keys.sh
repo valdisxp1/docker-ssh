7a33d4c9cc56edb49aecf66571e11e612f7b05b6
Valdis <valdisxp1@gmail.com>
2016-07-06 17:50:04 +0300

    No need to check for empty keys here, it is already tested by ssh-keygen

--- update_keys.sh
+++ update_keys.sh
@@ -5,12 +5,6 @@ then
 	AUTHORIZED_KEYS="`wget -qO- $AUTHORIZED_KEYS_URL`"
 fi
 
-if [ -z "${AUTHORIZED_KEYS}" ]
-then
-	>&2 echo "ERROR: make sure \$AUTHORIZED_KEYS contains at least 1 key"
-	exit 1
-fi
-
 echo "${AUTHORIZED_KEYS}" | tr , "\n" > /root/.ssh/authorized_keys.new
 
 # check if the keys are valid
@@ -18,7 +12,6 @@ ssh-keygen -lf /root/.ssh/authorized_keys.new
 if [ $? -ne 0 ]
 then
 	>&2 echo "ERROR: the new authorized_keys file are not valid, SSH keys will not be changed"
-	exit 1
 fi
 
 # authorized_keys file is valid, replace the old one
